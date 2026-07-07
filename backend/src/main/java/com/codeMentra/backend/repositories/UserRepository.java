19edcf7b70d347f5cf7a49b1ebb8cd4e06a52949
Riya <riya.singh.33.xth@gmail.com>
2026-07-07 11:31:01 +0530

    Implement JWT authentication and Spring Security configuration.

--- backend/src/main/java/com/codeMentra/backend/repositories/UserRepository.java
+++ backend/src/main/java/com/codeMentra/backend/repositories/UserRepository.java
@@ -3,5 +3,9 @@ package com.codeMentra.backend.repositories;
 import com.codeMentra.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsernameOrEmail(String username, String email);
+
 }
---
+++ backend/src/main/java/com/codeMentra/backend/security/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.codeMentra.backend.security;
+
+
+import com.codeMentra.backend.entity.User;
+import com.codeMentra.backend.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException{
+        User user = userRepository
+                .findByUsernameOrEmail(usernameOrEmail, usernameOrEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + usernameOrEmail));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPasswordHash(),
+                List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()))
+        );
+    }
+}
